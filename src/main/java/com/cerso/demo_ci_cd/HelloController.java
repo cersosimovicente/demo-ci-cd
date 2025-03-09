24dd1f2647cc1650c41f2953c954bf076f2af4e7
cerso <cersosimovicente@gmail.com>
2025-03-09 17:11:27 -0300

    commit 1

---
+++ src/main/java/com/cerso/demo_ci_cd/HelloController.java
@@ -0,0 +1,14 @@
+package com.cerso.demo_ci_cd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hola, mundo!";
+    }
+}
+
